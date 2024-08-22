16fd0c326d9827eb5ba729d879084afb21e87bd5
Karan <karanmaurya@gmail.com>
2024-08-22 20:28:05 +0530

    serving html file to home

--- utils/routes.js
+++ utils/routes.js
@@ -1,4 +1,5 @@
 import { User } from "../server/user.js";
+import path from 'path';
 
 class Routes {
 
@@ -7,20 +8,18 @@ class Routes {
 		Object.assign(this, http);
 	
 		this.http = http;
-
 		this.router = router;
 	}
 
 	async setupRoutes() {
 		
 		this.router.get('/', (req, res) => {
-			return res.status(200).send(`<h1>HI</h1>`)
+			return res.sendFile(path.join(this.__dirname, '../web/index.html'))
 		});
 		
 		this.router.get('/api/users', async(req, res) => {
-			const user = new User(this.mysql)
-			const userD = await user.execute();
-			return res.send(userD);
+			const user = this.http.call(`server/users`)
+			return res.send(user);
 		});
 	}
 }
